10467f9ac38d26b71e25f49e7864142a833878d5
H M Shanmukha Kumar <shanmukha.shanu.hm@gmail.com>
2021-03-24 14:29:27 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { CartComponent } from './cart/cart.component';
 import { ListingComponent } from './listing/listing.component';
 import { LandingComponent } from './landing/landing.component';
 import { NgModule } from '@angular/core';
@@ -7,11 +8,12 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path:'', redirectTo:'home',pathMatch:'full'},
   {path:'home',component:ListingComponent},
-  {path:'landing/:id',component:LandingComponent} 
+  {path:'landing/:id',component:LandingComponent},
+  {path:'cart',component:CartComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
-})
+}) 
 export class AppRoutingModule { }
